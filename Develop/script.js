bd73b99ac7e3666489b47d022ed24f2d489e5416
Anthony Vaquer <anthonymark126@gmail.com>
2023-06-09 07:17:40 -0700

    cleaned up some unused code and deleting some things that i left in here for reference

--- Develop/script.js
+++ Develop/script.js
@@ -1,44 +1,30 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(document).ready(function () {
   // Display current time/date
   var currentDate = dayjs().format("dddd, MMMM D, YYYY");
   $("#currentDay").text(currentDate);
 
   $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-
+    // event listener for saving event
     $(".saveBtn").on("click", function () {
       var event = $(this).siblings(".description").val();
       console.log($(this).parent());
       var time = $(this).parent().attr("id");
 
-      // console.log({
-      //   time,
-      //   event,
-      // });
-
-      const eventDetails = {
-        id: time,
-        time,
-        event,
-      }; 
       let prevsEvents = JSON.parse(localStorage.getItem("events"));
 
       if (prevsEvents !== null) {
         // prevsEvents.push(eventDetails);
 
-        // check if item already exists
-
+        // check if event exists
         const foundEvent = prevsEvents.find((eventItem) => {
           if (eventItem.id === time) {
             return eventItem;
           }
         });
-
+        // update any existing events
         if (foundEvent) {
-          const udpatedEvents = prevsEvents.map((item) => {
+          const udpatedEvents = prevs;
+          Events.map((item) => {
             if (item.id === time) {
               item.event = event;
             }
@@ -46,37 +32,39 @@ $(document).ready(function () {
 
             return item;
           });
-
+// save updated events to local storage
           localStorage.setItem("events", JSON.stringify(udpatedEvents));
           alert("Your event has been updated.");
           return;
         }
-
+// save previous events to local storage
         localStorage.setItem("events", JSON.stringify(prevsEvents));
       } else {
-        prevsEvents = [];
-        prevsEvents.push(eventDetails);
         localStorage.setItem("events", JSON.stringify(prevsEvents));
       }
       localStorage.setItem(time, event);
       $("description").val(localStorage.getItem("description"));
       alert("Your event has been saved.");
     });
-
   });
-
+  // run getTime to update time-blocks and retrieve any saved events
   getTime();
 });
 
-
 // gets current time and updates time-block colors
 function getTime() {
-
   var currentHour = dayjs().hour();
 
   $(".time-block").each(function () {
+    let saveEvent = localStorage.getItem($(this).attr("id"));
+    if (saveEvent) {
+      $(this).find(".description").val(saveEvent);
+      console.log(saveEvent);
+    }
     var hour = parseInt($(this).attr("id").split("-")[1]);
+    $(this).find(".description").val(saveEvent);
 
+    // changes timeblock class by comparing time block hour to current hour
     if (hour === currentHour) {
       $(this).removeClass("past");
       $(this).addClass("present");
